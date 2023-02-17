3bface98b0c4cb1d2234d45e38a6ca1988fc4a4c
Vuk <vukdavid@gmail.com>
2023-02-16 20:21:17 -0800

    5_8 & 3_9 & 12_22 I imam 20 poena

--- build/src/_new.d
+++
@@ -1,5 +0,0 @@
-build/src/_new.o: src/_new.cpp src/../h/my_mem.h src/../h/../lib/hw.h
-
-src/../h/my_mem.h:
-
-src/../h/../lib/hw.h:
--- build/src/my_mem.d
+++
@@ -1,3 +0,0 @@
-build/src/my_mem.o: src/my_mem.c src/../lib/hw.h
-
-src/../lib/hw.h:
